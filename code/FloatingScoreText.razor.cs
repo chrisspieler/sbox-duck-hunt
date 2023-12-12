86119bd9e17f8738c903162a7114484ffd54ad12
ducc <spielerc@gmail.com>
2023-12-12 09:15:13 -0700

    Add SwayFactor

--- code/FloatingScoreText.razor.cs
+++ code/FloatingScoreText.razor.cs
@@ -9,6 +9,7 @@ public partial class FloatingScoreText : PanelComponent
 	[Property] public float FloatAwayDistance { get; set; } = 200f;
 	[Property] public float FloatAwayTime { get; set; } = 1.5f;
 	[Property, Range( 0, 1 )] public float FadeInStart { get; set; } = 0.25f;
+	[Property, Range( 0, 50, 1)] public float SwayFactor { get; set; } = 5f;
 	[Property] public Vector3 StartPosition { get; set; }
 
 	private TimeSince _floatStart;
@@ -32,7 +33,7 @@ public partial class FloatingScoreText : PanelComponent
 		var easedProgress = Easing.QuadraticIn( currentProgress );
 		var currentHeight = MathX.Lerp( StartPosition.z, targetHeight, easedProgress );
 		Transform.Position = StartPosition
-			.WithY( StartPosition.y + MathF.Sin( Time.Now * MathF.PI ) )
+			.WithY( StartPosition.y + MathF.Sin( Time.Now * MathF.PI ) * SwayFactor )
 			.WithZ( currentHeight );
 		
 		if ( easedProgress > FadeInStart )
