1f956e07fd29fb1fddaef0a23bfda231bd1f1939
ducc <spielerc@gmail.com>
2024-10-11 16:56:47 -0600

    Add more boons

---
+++ code/UI/Screens/TikTokOverlay.razor.cs
@@ -0,0 +1,14 @@
+﻿using Sandbox;
+using System;
+
+public partial class TikTokOverlay : PanelComponent
+{
+	[Property] public bool ShowBars { get; set; } = false;
+
+	private string FillerClass => ShowBars ? "expanded" : string.Empty;
+
+	protected override int BuildHash()
+	{
+		return HashCode.Combine( ShowBars );
+	}
+}
