03fc87d94421dc3b3fbb77bd5a8df8105650817a
ducc <spielerc@gmail.com>
2024-11-15 12:10:30 -0700

    Add Category to Boon

--- code/Boon.cs
+++ code/Boon.cs
@@ -4,7 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-[GameResource( "Boon", "boon", "A game pass with additional information specific to Ducc Hunt", Icon = "emoji_events")]
+[GameResource( "Boon", "boon", "A game pass with additional information specific to Ducc Hunt", Icon = "emoji_events", Category = "s&bux")]
 public class Boon : GamePass
 {
 	[ConVar("boon_debug")] 
